24976f194d0752edd82b12d7755f56eda0349ec9
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2024-12-24 18:33:32 +0530

    Add error module

---
+++ src/components/error.rs
@@ -0,0 +1,15 @@
+
+use std::fmt::{Debug, Formatter};
+
+/// Error type for when a cell is accessed out of bounds on a board.
+pub struct OutOfBoundsSetError {
+    pub x: usize,
+    pub y: usize,
+    pub width: usize,
+    pub height: usize,
+}
+impl Debug for OutOfBoundsSetError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Out of bounds error: ({}, {}) accessed a board of size ({}, {})", self.x, self.y, self.width, self.height)
+    }
+}
