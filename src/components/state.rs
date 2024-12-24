31829f707408353fcb7c6dc90536da9e2366838c
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2024-12-24 18:32:30 +0530

    Add state trait and corresponding tests

--- src/components/state.rs
+++ src/components/state.rs
@@ -1,10 +1,14 @@
 use std::fmt::Debug;
 
+/// The `State` trait is used to define the possible states of a cell in a cellular automaton.
+/// 
+/// The trait implements Clone, Copy, PartialEq, Eq, Debug, Send, Sync, and 'static for Bevy compatibility.
 pub trait State: Clone + Copy + PartialEq + Eq + Debug + Send + Sync + 'static {}
 
 pub mod common_states {
     use super::State;
 
+    /// State representation for the Game of Life cellular automaton.
     #[derive(Clone, Copy, PartialEq, Eq, Debug)]
     pub enum GameOfLifeState {
         Dead,
@@ -13,6 +17,7 @@ pub mod common_states {
 
     impl State for GameOfLifeState {}
 
+    /// State representation for the Langton's Ant cellular automaton.
     #[derive(Clone, Copy, PartialEq, Eq, Debug)]
     pub enum LangtonsAntState {
         Empty,
