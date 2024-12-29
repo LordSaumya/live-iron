9b48cb8165773fe1995632dcb27359f129c97eb3
LordSaumya <68504323+LordSaumya@users.noreply.github.com>
2024-12-29 14:54:35 +0530

    Modify Langton's Ant struct to add ant direction.

--- src/components/state.rs
+++ src/components/state.rs
@@ -17,11 +17,35 @@ pub mod common_states {
 
     impl State for GameOfLifeState {}
 
-    /// State representation for the Langton's Ant cellular automaton.
-    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-    pub enum LangtonsAntState {
-        Empty,
-        Ant,
+    /// State representation for Langton's Ant facing direction.
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+    pub enum AntDirection {
+        Up,
+        Right,
+        Down,
+        Left,
+    }
+
+    /// State representation for Langton's Ant cell colour.
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+    pub enum CellColour {
+        White,
+        Black,
+    }
+
+    /// Stores the cell colour and optionally an ant with a facing direction.
+    /// 
+    /// The ant direction is optional because the ant may not be present on the cell.
+    /// If the ant is not present, the ant direction is None.
+    /// 
+    /// # Fields
+    /// 
+    /// - `colour`: The colour of the cell.
+    /// - `ant_direction`: The direction the ant is facing, if present.
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+    pub struct LangtonsAntState {
+        pub colour: CellColour,
+        pub ant_direction: Option<AntDirection>,
     }
 
     impl State for LangtonsAntState {}
